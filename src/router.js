9ec8faaff4438ad5dd9dc5dff64ad133a8dcb5db
dungvt9691 <dungvt9691@gmail.com>
2019-07-01 11:06:25 +0700

    Adding authentication flow

--- src/router.js
+++ src/router.js
@@ -1,25 +1,62 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Dashboard from './views/Dashboard.vue';
+import SignIn from './views/SignIn.vue';
+import SignUp from './views/SignUp.vue';
+
+import userServices from '@/lib/userServices';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'HomeScreen',
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/dashboard',
+      name: 'DashboardScreen',
+      component: Dashboard,
+      meta: { auth: true },
+    },
+    {
+      path: '/sign-in',
+      name: 'SignInScreen',
+      component: SignIn,
+      meta: { auth: false },
+    },
+    {
+      path: '/sign-up',
+      name: 'SignUpScreen',
+      component: SignUp,
+      meta: { auth: false },
     },
   ],
 });
+
+router.beforeEach((to, _from, next) => {
+  const isSignedIn = userServices.isSignedIn();
+
+  if (to.meta && to.meta.auth !== undefined) {
+    if (to.meta.auth) {
+      if (!isSignedIn) {
+        router.push({ name: 'SignIn' });
+      } else {
+        next();
+      }
+    } else if (!isSignedIn) {
+      next();
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
